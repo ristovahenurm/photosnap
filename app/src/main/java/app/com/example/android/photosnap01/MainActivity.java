265227fc153e4aaa81885223408afc198d730d65
ristovahenurm <ristovahenurm@gmail.com>
2015-12-09 20:01:38 +0200

    Proovime

---
+++ app/src/main/java/app/com/example/android/photosnap01/MainActivity.java
@@ -0,0 +1,13 @@
+package app.com.example.android.photosnap01;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/app/com/example/android/photosnap01/PlanListViewActivity.java
@@ -0,0 +1,14 @@
+package app.com.example.android.photosnap01;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class PlanListViewActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_list_view);
+    }
+}
+
