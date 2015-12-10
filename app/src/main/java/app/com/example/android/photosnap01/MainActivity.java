9587c4867d154b2714ba9d3e1a8315c2803b5815
ristovahenurm <ristovahenurm@gmail.com>
2015-12-10 16:03:43 +0200

    List view lisatud. 3 faili. (1 java, 2 xml) MainAcitvity javas string, mida enne commitimist tõi märkuseks

--- app/src/main/java/app/com/example/android/photosnap01/MainActivity.java
+++ app/src/main/java/app/com/example/android/photosnap01/MainActivity.java
@@ -1,13 +1,38 @@
 package app.com.example.android.photosnap01;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends ActionBarActivity {
+import app.com.example.android.photosnap01.R;
+
+public class MainActivity extends ListActivity {
+
+    String[] itemname ={
+            "Kuusemetsa 22a",
+            "Pikksilma 34/3",
+            "Vabaduse puiestee põik 3a",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War",
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setListAdapter(new ArrayAdapter<String>(
+                this, R.layout.activity_plan_list_view,
+                R.id.firstLine, itemname));
     }
 }
